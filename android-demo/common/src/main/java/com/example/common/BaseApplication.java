fd8ec7612afc86d4eddd39b9f36a35740b98dac2
weiyu <qning168@126.com>
2021-06-01 23:29:53 +0800

    update android-demo main module

--- android-demo/common/src/main/java/com/example/common/BaseApplication.java
+++ android-demo/common/src/main/java/com/example/common/BaseApplication.java
@@ -3,7 +3,7 @@ package com.example.common;
 import android.app.Application;
 import android.content.Context;
 
-import com.alibaba.android.arouter.launcher.ARouter;
+//import com.alibaba.android.arouter.launcher.ARouter;
 
 public class BaseApplication extends Application {
     public static Context context;
@@ -12,9 +12,9 @@ public class BaseApplication extends Application {
     public void onCreate() {
         super.onCreate();
         context = this;
-        ARouter.openLog();
-        ARouter.openDebug();
-        ARouter.init(BaseApplication.this);
+//        ARouter.openLog();
+//        ARouter.openDebug();
+//        ARouter.init(BaseApplication.this);
 //        Utils.init(BaseApplication.this);
     }
     public static Context getContext(){
