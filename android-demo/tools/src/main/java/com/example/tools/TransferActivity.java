12abdae4aab9d060678e9f2189d0789bd29eecc1
weiyu <qning168@126.com>
2021-07-16 22:50:52 +0800

    update tools

--- android-demo/tools/src/main/java/com/example/tools/TransferActivity.java
+++ android-demo/tools/src/main/java/com/example/tools/TransferActivity.java
@@ -2,16 +2,50 @@ package com.example.tools;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.didi.drouter.annotation.Router;
+import com.example.common.BaseApplication;
+//import com.yanzhenjie.andserver.AndServer;
+//import com.yanzhenjie.andserver.Server;
+
+import java.net.InetAddress;
+import java.util.concurrent.TimeUnit;
 
 @Router(path = "/transfer_activity")
 public class TransferActivity extends AppCompatActivity {
+    private Context context = BaseApplication.getContext();
+    private TextView tv_start,tv_stop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transfer);
+        tv_start = (TextView)findViewById(R.id.server_start);
+        tv_stop = (TextView)findViewById(R.id.server_stop);
+        tv_start.setOnClickListener((v->{
+//            server.startup();
+        }));
+        tv_stop.setOnClickListener((v -> {
+//            server.shutdown();
+        }));
     }
+
+//   Server server = AndServer.webServer(context)
+//            .port(8080)
+//            .timeout(10, TimeUnit.SECONDS)
+//            .build();
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+//        if (server.isRunning()) {
+//            server.shutdown();
+//        }
+    }
+
 }
